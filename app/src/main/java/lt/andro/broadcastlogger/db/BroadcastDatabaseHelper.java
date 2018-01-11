9f0b7d04a5acf7e5a04a73ffadf374020fc3253f
Vilius Kraujutis <viliusk@gmail.com>
2018-01-11 14:35:51 +0200

    Cleanup: rename database file

--- app/src/main/java/lt/andro/broadcastlogger/db/BroadcastDatabaseHelper.java
+++ app/src/main/java/lt/andro/broadcastlogger/db/BroadcastDatabaseHelper.java
@@ -6,24 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BroadcastDatabaseHelper extends SQLiteOpenHelper {
 
-  private static final String DATABASE_NAME = "todotable.db";
-  private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "broadcasts.sqlite";
+    private static final int DATABASE_VERSION = 1;
 
-  public BroadcastDatabaseHelper(Context context) {
-    super(context, DATABASE_NAME, null, DATABASE_VERSION);
-  }
+    public BroadcastDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-  // Method is called during creation of the database
-  @Override
-  public void onCreate(SQLiteDatabase database) {
-    BroadcastTable.onCreate(database);
-  }
+    // Method is called during creation of the database
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        BroadcastTable.onCreate(database);
+    }
 
-  // Method is called during an upgrade of the database,
-  // e.g. if you increase the database version
-  @Override
-  public void onUpgrade(SQLiteDatabase database, int oldVersion,
-      int newVersion) {
-    BroadcastTable.onUpgrade(database, oldVersion, newVersion);
-  }
+    // Method is called during an upgrade of the database,
+    // e.g. if you increase the database version
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion,
+                          int newVersion) {
+        BroadcastTable.onUpgrade(database, oldVersion, newVersion);
+    }
 } 
