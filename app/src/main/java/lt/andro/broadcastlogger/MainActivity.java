b0b4e830e3b02292a9df088a76b1ef3084228b46
Vitalii Dmitriev <dmitvitalii@yandex-team.ru>
2018-03-16 18:44:46 +0300

    Fix missing Toolbar.
    * Move from FragmentActivity to AppCompatActivity;
    * Create Toolbar view and add it to the main activity layout;
    * Fix theme to NoActionBar and set a Toolbar as support action bar;

--- app/src/main/java/lt/andro/broadcastlogger/MainActivity.java
+++ app/src/main/java/lt/andro/broadcastlogger/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<C
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         LoaderManager supportLoaderManager = getSupportLoaderManager();
         supportLoaderManager.initLoader(0, null, this);
