945a19bd3d9a83f2f59c1f935dae85731518e9b1
Vitalii Dmitriev <dmit.vitalii@gmail.com>
2018-03-18 00:25:50 +0300

    ServiceManager only contains single extension function.
    * Make an extension function to toggle a service;
    * Add a static flag to the BroadcastMonitoringService to check
    whether it's running or not.

--- app/src/main/java/lt/andro/broadcastlogger/BroadcastMonitoringService.java
+++ app/src/main/java/lt/andro/broadcastlogger/BroadcastMonitoringService.java
@@ -26,6 +26,7 @@ public class BroadcastMonitoringService extends Service {
     private static final String TAG = BroadcastMonitoringService.class.getSimpleName();
     private static final int NOTIFICATION = 1;
     private AnyBroadcastReceiver mAnyBroadcastReceiver;
+    private static boolean sRunning;
 
     /*
      * (non-Javadoc)
@@ -35,6 +36,7 @@ public class BroadcastMonitoringService extends Service {
     public void onCreate() {
         super.onCreate();
         mAnyBroadcastReceiver = new AnyBroadcastReceiver();
+        sRunning = true;
     }
 
     /*
@@ -73,9 +75,14 @@ public class BroadcastMonitoringService extends Service {
                     "Skipping Exception which might be raising when the receiver is not yet registered");
 
         }
+        sRunning = false;
         hideNotification(getApplicationContext());
         Log.d(TAG, "onDestroy");
         super.onDestroy();
+    }
+
+    public static boolean isRunning() {
+        return sRunning;
     }
 
     private void registerAnyBroadcastReceiver() {
--- app/src/main/java/lt/andro/broadcastlogger/ServiceManager.kt
+++ app/src/main/java/lt/andro/broadcastlogger/ServiceManager.kt
@@ -1,28 +1,18 @@
 package lt.andro.broadcastlogger
 
-import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
-import java.lang.ref.WeakReference
 
 /**
- * Created by Vitalii Dmitriev
+ * Starts and stops a service, whose [Intent] was provided in `ServiceManager`'s
+ * constructor.
+ *
+ * @return true if the [BroadcastMonitoringService] starts, false otherwise.
  */
-class ServiceManager(private val serviceIntent: Intent, context: Context) {
-    private var launched = false
-
-    /**
-     * Starts and stops a service, whose [Intent] was provided in `ServiceManager`'s
-     * constructor.
-     */
-    fun toggleService(context: Context): Boolean {
-        launched = if (launched) {
-            context.stopService(serviceIntent)
-            false
-        } else {
-            context.startService(serviceIntent)
-            true
-        }
-        return launched
-    }
+fun Context.toggleService(intent: Intent) = if (BroadcastMonitoringService.isRunning()) {
+    this.stopService(intent)
+    false
+} else {
+    this.startService(intent)
+    true
 }
